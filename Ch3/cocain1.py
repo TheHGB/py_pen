86fe26b154044fb8c01df29b967eb7e6be7b54d6
TheHGB <HOLAEMDICHGB@gmail.com>
2017-07-01 12:38:41 +0200

    Added cocain1.py, a simple sniffer

---
+++ Ch3/cocain1.py
@@ -0,0 +1,31 @@
+# Get it?, because it is a sniffer (kys)
+# With this one you'll only analyze the first packet you intercept and the analysis will only be correct if the packet is TCP and the headers don't have any special option enabled
+
+import socket
+import binascii
+import struct
+
+s = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x800))
+
+while True:
+    intercept = s.recvfrom(2048)
+    eth_header = intercept[0][0:14] 
+    ip_header = intercept[0][14:34]
+    tcp_header = intercept[0][34:54]
+    eth_unpack = struct.unpack("!6s6s2s",eth_header)
+    ip_unpack = struct.unpack("12s4s4s", ip_header)
+    tcp_unpack = struct.unpack("!HH9ss6s", tcp_header)
+
+    print "---------Ethenrent header-----------"
+    print "destination MAC: ", binascii.hexlify(eth_unpack[0])
+    print "source MAC: ", binascii.hexlify(eth_unpack[1])
+    print "------------IP header--------------"
+    print "source IP: ", socket.inet_ntoa(ip_unpack[1])
+    print "destination IP: ", socket.inet_ntoa(ip_unpack[2])
+    print "------------TCP header--------------"
+    print "source port: ", tcp_unpack[0]
+    print "destination port: ", tcp_unpack[1]
+    print "flags: ", binascii.hexlify(tcp_unpack[3])
+
+
+
